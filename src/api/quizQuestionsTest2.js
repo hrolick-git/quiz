60a48bdfb591ffb11711ccedef8ddd97f7878450
Hrolick <alessandra.arisu@gmail.com>
2018-10-04 16:46:42 +0300

    Init animation

--- src/api/quizQuestionsTest2.js
+++ src/api/quizQuestionsTest2.js
@@ -313,6 +313,216 @@ var quizQuestionsTest2 = [
           ]
         },
       ]
+  },
+  {
+      question: "4.	З переліченого нижче мені найлегше:",
+      subquestions: [
+        {
+          question: "а)	знайти ідеальну для мене гучність звучання магнітофона, телевізора",
+          answers: [
+              {
+                  type: "Audi",
+                  content: "4",
+                  positive: true
+              },
+              {
+                  type: "Audi",
+                  content: "3",
+                  positive: true
+              },
+              {
+                  type: "Audi",
+                  content: "2",
+                  positive: false
+              },
+              {
+                  type: "Audi",
+                  content: "1",
+                  positive: false
+              }
+          ]
+        },
+        {
+          question: "б)	підібрати найбільш розумні, переконливі аргументи, пов'язані з цікавить мене предмету",
+          answers: [
+              {
+                  type: "Audi",
+                  content: "4",
+                  positive: true
+              },
+              {
+                  type: "Audi",
+                  content: "3",
+                  positive: true
+              },
+              {
+                  type: "Audi",
+                  content: "2",
+                  positive: false
+              },
+              {
+                  type: "Audi",
+                  content: "1",
+                  positive: false
+              }
+          ]
+        },
+        {
+          question: "в)	вибрати найбільш зручні меблі",
+          answers: [
+              {
+                  type: "Audi",
+                  content: "4",
+                  positive: true
+              },
+              {
+                  type: "Audi",
+                  content: "3",
+                  positive: true
+              },
+              {
+                  type: "Audi",
+                  content: "2",
+                  positive: false
+              },
+              {
+                  type: "Audi",
+                  content: "1",
+                  positive: false
+              }
+          ]
+        },
+        {
+          question: "г)	підібрати для одягу, інтер'єру та ін. багаті колірні комбінації",
+          answers: [
+              {
+                  type: "Audi",
+                  content: "4",
+                  positive: true
+              },
+              {
+                  type: "Audi",
+                  content: "3",
+                  positive: true
+              },
+              {
+                  type: "Audi",
+                  content: "2",
+                  positive: false
+              },
+              {
+                  type: "Audi",
+                  content: "1",
+                  positive: false
+              }
+          ]
+        },
+      ]
+  },
+  {
+      question: "5.	Що найбільше впливає на мій настрій, самопочуття:",
+      subquestions: [
+        {
+          question: "а)	я дуже чутливий до оточуючих мене звукам, шумів, інтонації голосів людей",
+          answers: [
+              {
+                  type: "Audi",
+                  content: "4",
+                  positive: true
+              },
+              {
+                  type: "Audi",
+                  content: "3",
+                  positive: true
+              },
+              {
+                  type: "Audi",
+                  content: "2",
+                  positive: false
+              },
+              {
+                  type: "Audi",
+                  content: "1",
+                  positive: false
+              }
+          ]
+        },
+        {
+          question: "б)	відчуваю себе «у своїй тарілці», коли мені треба довести правильність будь-якого положення, з'ясувати причину якої-небудь події, побудувати ланцюг логічних умовиводів",
+          answers: [
+              {
+                  type: "Audi",
+                  content: "4",
+                  positive: true
+              },
+              {
+                  type: "Audi",
+                  content: "3",
+                  positive: true
+              },
+              {
+                  type: "Audi",
+                  content: "2",
+                  positive: false
+              },
+              {
+                  type: "Audi",
+                  content: "1",
+                  positive: false
+              }
+          ]
+        },
+        {
+          question: "в)	я дуже чутливий до того, наскільки зручна мій одяг, чи приємно мені в ній перебувати, рухатися і інше",
+          answers: [
+              {
+                  type: "Audi",
+                  content: "4",
+                  positive: true
+              },
+              {
+                  type: "Audi",
+                  content: "3",
+                  positive: true
+              },
+              {
+                  type: "Audi",
+                  content: "2",
+                  positive: false
+              },
+              {
+                  type: "Audi",
+                  content: "1",
+                  positive: false
+              }
+          ]
+        },
+        {
+          question: "г)	на мене справляє сильне враження освітлення і загальний вигляд обстановки, приміщення",
+          answers: [
+              {
+                  type: "Audi",
+                  content: "4",
+                  positive: true
+              },
+              {
+                  type: "Audi",
+                  content: "3",
+                  positive: true
+              },
+              {
+                  type: "Audi",
+                  content: "2",
+                  positive: false
+              },
+              {
+                  type: "Audi",
+                  content: "1",
+                  positive: false
+              }
+          ]
+        },
+      ]
   }
 ];
 
--- src/components/Test_2.js
+++ src/components/Test_2.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import update from 'react-addons-update';
 import quizQuestionsTest2 from '../api/quizQuestionsTest2';
 import $ from 'jquery';
+import bodymovin from 'bodymovin';
+import * as animationData from '../svg/confetti.json';
+import * as animationDataWhal from '../svg/empty_status.json';
+import * as animationDataBox from '../svg/box.json';
 
 const initialState = {
   counter: 0,
@@ -17,24 +21,125 @@ const initialState = {
   },
   result: '',
   allAnswers: []
-  };
+};
 
-  class Test_2 extends Component {
+var animFireworks, animWhal, animBox;
+
+class Test_2 extends Component {
   constructor(props) {
     super(props);
     this.state = initialState;
     this.handleAnswerSelected = this.handleAnswerSelected.bind(this);
     this.summCountAnswer = this.summCountAnswer.bind(this);
+    this.handleAnswerClick = this.handleAnswerClick.bind(this);
   }
 
-  summCountAnswer() {
+  componentDidMount() {
+    animFireworks = bodymovin.loadAnimation({
+      container: document.getElementById('bm'), // the dom element
+      renderer: 'svg',
+      loop: false,
+      autoplay: false,
+      animationData: animationData, // the animation data
+      rendererSettings: {
+        scaleMode: 'noScale',
+        clearCanvas: false,
+        progressiveLoad: false, // Boolean, only svg renderer, loads dom elements when needed. Might speed up initialization for large number of elements.
+        hideOnTransparent: true, //Boolean, only svg renderer, hides elements when opacity reaches 0 (defaults to true)
+        className: 'some-css-class-name'
+      }
+    });
+    animWhal = bodymovin.loadAnimation({
+      container: document.getElementById('a-whal'), // the dom element
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: animationDataWhal, // the animation data
+      rendererSettings: {
+        scaleMode: 'noScale',
+        clearCanvas: false,
+        progressiveLoad: false, // Boolean, only svg renderer, loads dom elements when needed. Might speed up initialization for large number of elements.
+        hideOnTransparent: true, //Boolean, only svg renderer, hides elements when opacity reaches 0 (defaults to true)
+        className: 'anim_whal'
+      }
+    });
+    animBox = bodymovin.loadAnimation({
+      container: document.getElementById('a-box'), // the dom element
+      renderer: 'svg',
+      loop: false,
+      autoplay: false,
+      animationData: animationDataBox, // the animation data
+      rendererSettings: {
+        scaleMode: 'noScale',
+        clearCanvas: false,
+        progressiveLoad: false, // Boolean, only svg renderer, loads dom elements when needed. Might speed up initialization for large number of elements.
+        hideOnTransparent: true, //Boolean, only svg renderer, hides elements when opacity reaches 0 (defaults to true)
+        className: 'anim_box'
+      }
+    });
+    $(".show").on("click", function(){
+        animBox.setDirection(1);
+        $("#a-box").show();
+        animBox.play();
+    });
+    $(".hide").on("click", function(){
+        animBox.setDirection(-1);
+        animBox.play();
+        setTimeout(function(){
+          $("#a-box").hide();
+        }, 1500);
+    });
+  }
+
+  summCountAnswer(name) {
+    let audiFun = () => {
       var countAudi = 0;
       $("input[name^='Audi']:checked").each(function(input){
         countAudi = countAudi + parseInt($(this).val(),10);
       });
-       this.setState({
-         answersCount: {...this.state.answersCount, Audi: countAudi}
-       });
+      this.setState({
+        answersCount: {...this.state.answersCount, Audi: countAudi}
+      });
+    }
+    let visualFun = () => {
+      var countVisual = 0;
+      $("input[name^='Visual']:checked").each(function(input){
+        countVisual = countVisual + parseInt($(this).val(),10);
+      });
+      this.setState({
+        answersCount: {...this.state.answersCount, Visual: countVisual}
+      });
+    }
+    let kinestFun = () => {
+      var countKinest = 0;
+      $("input[name^='Kinest']:checked").each(function(input){
+        countKinest = countKinest + parseInt($(this).val(),10);
+      });
+      this.setState({
+        answersCount: {...this.state.answersCount, Kinest: countKinest}
+      });
+    }
+
+    switch (name) {
+      case (name.match(/Audi/)||{}).input:
+        return audiFun();
+      case (name.match(/Visual/)||{}).input:
+        return visualFun();
+      case (name.match(/Kinest/)||{}).input:
+        return kinestFun();
+      default:
+        return this.state;
+    }
+  }
+
+  handleAnswerClick(e) {
+    $("#bm").show();
+    $("#bm").offset({left:e.pageX-50,top:e.pageY-50});
+    animFireworks.play();
+    setTimeout(function(){
+      animFireworks.stop();
+      $("#bm").hide();
+    }, 1500);
   }
 
   handleAnswerSelected(e){
@@ -42,24 +147,30 @@ const initialState = {
     change[e.target.name] = e.target.value;
     console.log(change);
     this.setState(change);
-    this.summCountAnswer();
+    this.summCountAnswer(e.target.name);
     console.log(this.state.answersCount);
   }
+
 
   render() {
     return (
       <div className="container">
+          <div id="bm"></div>
+        <div className="row">
+        </div>
         <div className="row">
           <div className="col-12 text-center justify-content-center">
-              <p className="quiz_subtitle d-block mb-sm-5 col-12 mt-3">Діагностика домінуючої перцептивної модальності</p>
+              <p className="quiz_subtitle d-block mb-sm-5 col-12 mt-3">БІАС-тест для визначення репрезентативних систем</p>
           </div>
         </div>
         <div className="row reverso-sm">
           <div className="col-12">
             <div className="row text-center justify-content-center">
-                  <div className="col-12 col-md-4">
-                  <img className="quiz_info__img" src="./svg/test-header-1.svg" alt=""/>
-                <p className="quiz_info__text mt-3">Тест займає менше 5 хвилин.</p>
+                <div className="col-12 col-md-4">
+                  {/* <img className="quiz_info__img" src="./svg/test-header-1.svg" alt=""/> */}
+                  <div id="a-box" className="hide"></div>
+                  <div id="a-whal" className="mx-auto show"></div>
+                  <p className="quiz_info__text mt-3">Тест займає менше 5 хвилин.</p>
                 </div>
                 <div className="col-12 col-md-4">
                   <img className="quiz_info__img" src="./svg/test-header-2.svg" alt=""/>
@@ -90,6 +201,7 @@ const initialState = {
                                 className="quiz_answer__input"
                                 type="radio"
                                 value={answer.content}
+                                onClick={this.handleAnswerClick}
                                 onChange={this.handleAnswerSelected}
                                 name={answer.type+"-"+i1+i2}
                                 id={answer.type+"-"+i1+i2+i3}
@@ -109,7 +221,13 @@ const initialState = {
             </div>
           );
         })}
+      <hr/>
+      <div className="row">
+        <div className="col-12 text-center">
+        <button type="button" disabled>Перевірити результат</button>
+        </div>
       </div>
+    </div>
     );
   }
 
