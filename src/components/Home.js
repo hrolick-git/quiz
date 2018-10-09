912d1fabd945d539cd5db84a2edc648816f5fb90
Hrolick <alessandra.arisu@gmail.com>
2018-10-09 18:16:11 +0300

    init

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,88 +1,10 @@
 import React, { Component } from 'react';
-import {FB, FacebookApiException} from 'fb';
-
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.checkLoginState = this.checkLoginState.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.testAPI = this.testAPI.bind(this);
-    this.statusChangeCallback = this.statusChangeCallback.bind(this);
-  }
-  componentDidMount() {
-    window.fbAsyncInit = function() {
-      FB.init({
-        appId      : '2109493355770108',
-        cookie     : true,
-        xfbml      : true,
-        version    : 'v2.8'
-      });
-      FB.AppEvents.logPageView();
-      FB.Event.subscribe('auth.statusChange', function(response) {
-        if (response.authResponse) {
-          this.checkLoginState();
-        } else {
-          console.log('---->User cancelled login or did not fully authorize.');
-        }
-      }.bind(this));
-    }.bind(this);
-
-    // Load the SDK asynchronously
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = '//connect.facebook.net/en_US/sdk.js';
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-  }
-
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
-  testAPI() {
-    console.log('Welcome! Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
-    });
-  }
-
-  // This is called with the results from from FB.getLoginStatus().
-  statusChangeCallback(response) {
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      this.testAPI();
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
-    } else {
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-      'into Facebook.';
+export default class Home extends Component {
+    render() {
+      return (
+        <div>
+          Home
+        </div>
+      );
     }
-  }
-
-  checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      this.statusChangeCallback(response);
-    }.bind(this));
-  }
-
-  handleClick() {
-    FB.login(this.checkLoginState());
-  }
-
-  render() {
-    return (
-      <main>
-        <a href="#" onClick={this.handleClick} onlogin={this.checkLoginState}>Login</a>
-        <div id="status"></div>
-      </main>
-    );
-  }
 }
-
-export default Home;
